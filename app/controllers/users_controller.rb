fce0c4aff774a1522a0506065519658b55e02d31
makuto114 <35193508+makuto114@users.noreply.github.com>
2021-02-17 19:30:29 +0900

    ビュー微修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,8 @@ class UsersController < ApplicationController
       render action: :new
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
   get 'items/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
 end
